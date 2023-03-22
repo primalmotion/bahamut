8f480cb35444fd36e5de592a00db750dbf46a14e
primalmotion <208711+primalmotion@users.noreply.github.com>
2023-03-21 21:44:11 -0700

    Update meta.go

--- meta.go
+++ meta.go
@@ -132,6 +132,7 @@ func buildVersionedRoutes(modelManagers map[int]elemental.ModelManager, processo
 	return versionedRoutes
 }
 
+// BuildVersionedRoutes is useless
 func BuildVersionedRoutes(modelManagers map[int]elemental.ModelManager, processorFinder processorFinderFunc) map[int][]RouteInfo {
 
 	addRoute := func(routes map[string]routeBuilder, identity elemental.Identity, url string, verb string, private bool) {
